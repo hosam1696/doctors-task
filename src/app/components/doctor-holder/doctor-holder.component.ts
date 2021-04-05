a898e43ee73f1c1a0fd81c253de881c6c1fcae23
hosamsam <hosamelnabawy1996@gmail.com>
2021-04-05 13:17:04 +0200

    Initial commit

---
+++ src/app/components/doctor-holder/doctor-holder.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-holder',
+  templateUrl: './doctor-holder.component.html',
+  styleUrls: ['./doctor-holder.component.scss'],
+})
+export class DoctorHolderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
